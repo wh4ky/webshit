ff308713cbcbbf59e73109914ffc19c6935c096b
wh4ky <damian.czapnik360@outlook.com>
2023-11-26 17:42:14 +0100

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 (function() {
-  const button = document.querySelector("#theme") // pakt eerste butotn die het kan vinden button, # betekent id, . betekend class
+  const button = document.querySelector("#nav#theme") // pakt eerste butotn die het kan vinden button, # betekent id, . betekend class
   const everything = document.querySelectorAll("body *") // selects alles
 
   let lightMode = false;
