d94af98372752ac8b8ca56559250aa5419bc24db
jsw <jurnwubben@gmail.com>
2023-11-26 17:46:50 +0100

    code formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
-(function() {
-  const button = document.querySelector("#nav#theme") // pakt eerste butotn die het kan vinden button, # betekent id, . betekend class
-  const everything = document.querySelectorAll("body, body > *") // selects alles
-
+(function () {
+  const button = document.querySelector("#nav#theme"); // pakt eerste butotn die het kan vinden button, # betekent id, . betekend class
+  const everything = document.querySelectorAll("body, body > *"); // selects alles
 
   let lightMode = false;
 
-  button.addEventListener("click", function() {
-    lightMode = !lightMode
-   
-    lightMode ? everything.forEach(v => v.classList.add("light")) : everything.forEach(v => v.classList.remove("light"))
-  })
-})()
+  button.addEventListener("click", function () {
+    lightMode = !lightMode;
+
+    lightMode
+      ? everything.forEach((v) => v.classList.add("light"))
+      : everything.forEach((v) => v.classList.remove("light"));
+  });
+})();
