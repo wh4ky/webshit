181130f4dd5e1fd985be8ff478c8d49ca91e669a
jsw <jurnwubben@gmail.com>
2023-11-26 17:40:30 +0100

    background light mode fix 2.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 (function() {
   const button = document.querySelector("#theme") // pakt eerste butotn die het kan vinden button, # betekent id, . betekend class
-  const everything = document.querySelectorAll("body *") // selects alles
+  const everything = document.querySelectorAll("body, body > *") // selects alles
 
   let lightMode = false;
 
